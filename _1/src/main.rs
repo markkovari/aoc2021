8de8cfbb03a2eee13fb439b87d208cc95b578e9c
Márk Kővári <mkovari@diligent.com>
2021-12-01 10:24:52 +0100

    feat: calculate window depth growings

--- _1/src/main.rs
+++ _1/src/main.rs
@@ -9,6 +9,19 @@ fn calculate_growings(numbers: Vec<u64>) -> u64 {
     }
     count
 }
+
+fn calcualte_window_growings(numbers: Vec<u64>) -> u64 {
+    let mut count = 0;
+    for i in 0..(numbers.len() - 3) {
+        let window_sum: u64 = numbers[i] + numbers[i + 1] + numbers[i + 2];
+        let next_window_sum: u64 = numbers[i + 1] + numbers[i + 2] + numbers[i + 3];
+        if next_window_sum > window_sum {
+            count += 1;
+        }
+    }
+    count
+}
+
 fn main() {
     let file_name = "input.txt";
     let content = fs::read_to_string(file_name).expect("Something went wrong reading the file");
@@ -17,5 +30,6 @@ fn main() {
         .filter_map(|line| line.trim().parse::<u64>().ok())
         .collect::<Vec<u64>>();
 
-    println!("{}", calculate_growings(depths));
+    println!("{}", calculate_growings(depths.clone()));
+    println!("{}", calcualte_window_growings(depths));
 }
