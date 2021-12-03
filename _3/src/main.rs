bc2c960e6316332d0808a8f3d90594d3f5ba143a
Márk Kővári <mkovari@diligent.com>
2021-12-03 09:52:28 +0100

    feat: reading binary numbers sheeeesh ft

--- _3/src/main.rs
+++ _3/src/main.rs
@@ -1,4 +1,5 @@
 fn main() {
     let content = include_str!("../input.test");
-    println!("file content: {}", content);
+    let binary_numbers: Vec<Vec<u8>> = content.lines().map(|line| line.split("").filter_map(|e| e.parse::<u8>().ok()).collect()).collect();
+    println!("{:?}", binary_numbers);
 }
