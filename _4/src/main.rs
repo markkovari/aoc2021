68b00a0e46895a052618456194a05256f7b1eae8
Márk Kővári <mkovari@diligent.com>
2021-12-04 21:34:14 +0100

    refactor: add fromstring to table

--- _4/src/main.rs
+++ _4/src/main.rs
@@ -7,8 +7,7 @@ struct Elem(u8, bool);
 impl FromStr for Elem {
     type Err = ParseIntError;
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let num = s.parse::<u8>()?;
-        Ok(Elem(num, false))
+        Ok(Elem(s.parse::<u8>()?, false))
     }
 }
 
@@ -18,16 +17,29 @@ struct Row(Vec<Elem>, bool);
 impl FromStr for Row {
     type Err = ParseIntError;
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let elements = s
-            .split(" ")
-            .filter_map(|elem| Elem::from_str(elem).ok())
-            .collect::<Vec<Elem>>();
-        Ok(Row(elements, false))
+        Ok(Row(
+            s.split(" ")
+                .filter_map(|elem| Elem::from_str(elem).ok())
+                .collect::<Vec<Elem>>(),
+            false,
+        ))
     }
 }
 
 #[derive(Debug)]
 struct Table(Vec<Row>, bool);
+
+impl FromStr for Table {
+    type Err = ParseIntError;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(Table(
+            s.split("\n")
+                .filter_map(|row| Row::from_str(row).ok())
+                .collect::<Vec<Row>>(),
+            false,
+        ))
+    }
+}
 
 fn main() {
     let content = include_str!("../input.test")
@@ -42,15 +54,7 @@ fn main() {
 
     let tables: Vec<Table> = (&content[1..])
         .into_iter()
-        .map(|&table| {
-            Table(
-                table
-                    .split("\n")
-                    .filter_map(|row| Row::from_str(row).ok())
-                    .collect::<Vec<Row>>(),
-                false,
-            )
-        })
+        .filter_map(|&table| Table::from_str(table).ok())
         .collect::<Vec<Table>>();
     println!("{:?}", tables);
     println!("{:?}", choosen_numbers);
