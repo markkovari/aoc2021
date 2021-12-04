4d2fe906922508852750402a6b7108e248264678
Márk Kővári <mkovari@diligent.com>
2021-12-04 21:11:15 +0100

    feat: read elements with default not found state

--- _4/src/main.rs
+++ _4/src/main.rs
@@ -1,3 +1,6 @@
+#[derive(Debug)]
+struct Elem(u8, bool);
+
 fn main() {
     let content = include_str!("../input.test")
         .split("\n\n")
@@ -9,5 +12,22 @@ fn main() {
         .filter_map(|e| e.parse::<u8>().ok())
         .collect::<Vec<u8>>();
 
+    let tables_raw = &content[1..];
+
+    let tables: Vec<Vec<Vec<Elem>>> = tables_raw
+        .into_iter()
+        .map(|&table| {
+            table
+                .split("\n")
+                .map(|row| {
+                    row.split(" ")
+                        .filter_map(|elem| elem.parse::<u8>().ok())
+                        .map(|e| Elem(e, false))
+                        .collect::<Vec<Elem>>()
+                })
+                .collect::<Vec<Vec<Elem>>>()
+        })
+        .collect::<Vec<Vec<Vec<Elem>>>>();
+    println!("{:?}", tables);
     println!("{:?}", choosen_numbers);
 }
