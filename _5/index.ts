50a601086db2e6fecf6771a02f8b9d0df160821b
Márk Kővári <mkovari@diligent.com>
2021-12-10 23:18:02 +0100

    feat: day 5 a

--- _5/index.ts
+++ _5/index.ts
@@ -1,138 +1,57 @@
-const content = (await Deno.readTextFile("./input.test")).split("\n");
+const content = (await Deno.readTextFile("./input.data")).split("\n").filter((
+  line: string,
+) => line.length !== 0);
 
-type Value = "." | number;
+console.log({ content });
+// for()
 
-class Point {
-  horizontal = 0;
-  vertical = 0;
-  value: Value = ".";
-
-  constructor() {}
-
-  static fromString(from: string) {
-    const [h, v] = from.split(",");
-    return this.fromValues(+h, +v, ".");
-  }
-  static fromValues(horizontal: number, vertical: number, val: Value): Point {
-    const point: Point = new Point();
-    point.vertical = vertical;
-    point.horizontal = horizontal;
-    point.value = val;
-    return point;
-  }
+function lineSplitter(from: string): [number[], number[]] {
+  const [start, end] = from.split(" -> ");
+  const [startX, startY] = start.split(",").map(Number);
+  const [endX, endY] = end.split(",").map(Number);
+  return [[startX, startY], [endX, endY]];
 }
 
-class Line {
-  start: Point;
-  end: Point;
-
-  constructor(from: string) {
-    const [start, end] = from.split(" -> ");
-    this.start = Point.fromString(start);
-    this.end = Point.fromString(end);
-  }
-
-  get isHorizontal() {
-    return this.start.horizontal === this.end.horizontal;
-  }
-  get isVertical() {
-    return this.start.vertical === this.end.vertical;
-  }
-
-  normalise() {
-    if (this.isHorizontal) {
-      if (this.end.vertical < this.start.vertical) {
-        const [start, end] = [this.end, this.start];
-        this.start = start;
-        this.end = end;
-      }
-    }
-    if (this.isVertical) {
-      if (this.end.horizontal < this.start.horizontal) {
-        const [start, end] = [this.end, this.start];
-        this.start = start;
-        this.end = end;
-      }
-    }
-    return this;
-  }
+function horizontalOrVertical(of: [number[], number[]]): boolean {
+  return of[0][0] === of[1][0] || of[0][1] === of[1][1];
 }
 
-class PointMap {
-  points: Point[][] = [];
-  lines: Line[];
-  constructor(lines: Line[]) {
-    this.lines = lines;
-    const maxHorizontal: number = Math.max(
-      ...lines.map(({ end }) => end.horizontal),
-    );
-    const maxVertical: number = Math.max(
-      ...lines.map(({ end }) => end.vertical),
-    );
-    this.points = [...Array(maxHorizontal).keys()]
-      .map((i: number) =>
-        [...Array(maxVertical).keys()].map((j: number) =>
-          Point.fromValues(i, j, ".")
-        )
-      );
-    for (let i = 0; i < maxHorizontal; i++) {
-      for (let j = 0; j < maxVertical; j++) {
-        this.points[i][j] = Point.fromValues(i, j, ".");
-      }
+const neededLines = content.map(lineSplitter).filter(horizontalOrVertical);
+console.log(neededLines);
+
+const maxX = Math.max(...neededLines.map((e) => Math.max(e[0][0], e[1][0])));
+const maxY = Math.max(...neededLines.map((e) => Math.max(e[0][1], e[1][1])));
+console.log({ maxX, maxY });
+
+const pointMap: number[][] = Array.from(
+  Array(maxX + 1),
+  (_) => Array(maxY + 1).fill(0),
+);
+
+for (const [start, end] of neededLines) {
+  if (start[0] === end[0]) {
+    const [startPoint, endPoint] = start[1] < end[1]
+      ? [start[1], end[1]]
+      : [end[1], start[1]];
+    console.log("horizontal:", { startPoint, endPoint, x: start[0] });
+    for (let i = startPoint; i <= endPoint; i++) {
+      pointMap[i][start[0]] += 1;
     }
   }
 
-  get verticalLines() {
-    return this.lines.filter((line: Line) => line.isHorizontal).map((
-      line: Line,
-    ) => line.normalise());
-  }
+  if (start[1] === end[1]) {
+    const [startPoint, endPoint] = start[0] < end[0]
+      ? [start[0], end[0]]
+      : [end[0], start[0]];
+    console.log("horizontal:", { startPoint, endPoint, y: start[0] });
 
-  get horizontalLines() {
-    return this.lines.filter((line: Line) => line.isVertical).map((
-      line: Line,
-    ) => line.normalise());
-  }
-
-  get straightLines() {
-    return [...this.horizontalLines, ...this.verticalLines];
-  }
-
-  addLineValues() {
-    console.log("beforeVLinesapply", this.points);
-    for (const { start, end } of this.verticalLines) {
-      for (let i = start.vertical; i <= end.vertical; i++) {
-        const point = this.points[i][start.horizontal];
-        console.log("vLine:", this.points[i]);
-        point.value = point.value === "." ? 1 : point.value + 1;
-      }
+    for (let i = startPoint; i <= endPoint; i++) {
+      pointMap[start[1]][i] += 1;
     }
-    console.log("afterVLinesapply", this.points);
-    for (const { start, end } of this.horizontalLines) {
-      for (let i = start.horizontal; i <= end.horizontal; i++) {
-        console.log({ x: start.horizontal, i, a: start.vertical });
-        console.log("line", this.points[start.vertical]);
-        const point = this.points[start.vertical][i];
-        console.log({ point, x: start.horizontal, y: i });
-        point.value = point.value === "." ? 1 : point.value + 1;
-      }
-    }
-    console.log("afterHLinesapply", this.points);
-  }
-
-  toString(): string {
-    return this.points.map((line: Point[]) =>
-      line.map(({ value }) => value).join(" ")
-    ).join(" \n");
   }
 }
+console.table(pointMap);
+const countOfAboveOne = pointMap.map((v) => v.filter((e) => e > 1).length)
+  .reduce((a, b) => a + b, 0);
 
-const lines = content.map((line: string) => new Line(line));
-
-const pointMap = new PointMap(lines);
-
-// console.log({ content, lines, pointMap: pointMap.toString() });
-console.log("vertical lines", pointMap.verticalLines);
-// console.log(pointMap.toString());
-pointMap.addLineValues();
-console.log(pointMap.toString());
+console.log(countOfAboveOne);
